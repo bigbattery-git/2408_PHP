09672c5cff3a4e68125551819f190a5163eb80f5
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-09-12 16:39:47 +0900

    레퍼런스

--- php/Ex/064_passing_by_reference.php
+++ php/Ex/064_passing_by_reference.php
@@ -27,6 +27,23 @@
   }
 
   $testNum = 1;
-  test($testNum);
+  test2($testNum);
 
   echo $testNum; // 0
+
+  // 스코프 : 변수나 함수의 유효 범위
+
+  // 전역 스코프
+
+  $intA = 10;     // 전역 스코프
+  $strA = "abc";  // 전역 스코프
+
+  function TestTest($testParam){
+    global $intA;   // 지역 스코프 영역에서 전역 스코프 변수를 사용하려면 global로 선언해야 함
+    echo $intA;
+
+    $intB = 20;     // 지역 스코프
+    $strB = "def";  // 지역 스코프
+  }
+
+  TestTest(1);
