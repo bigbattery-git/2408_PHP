2d454e9c5852cd233f62251da8a093c7f8dd93fa
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-10-29 10:04:15 +0900

    js math

---
+++ javascript/js/29.js
@@ -0,0 +1,13 @@
+// Math 객체
+
+Math.ceil(0.5);                      // 올림    // 1
+Math.round(0.5);                     // 반올림  // 1
+Math.floor(0.5);                     // 내림    // 0
+
+Math.random();                       // 0 ~ 1 사이의 랜덤값
+Math.floor(Math.random() * 10) + 1;  // 1 ~ 10 사이의 랜덤값
+
+Math.max(1,2,3,4,5);                 // 파라미터 내 최대값 반환 // 5
+Math.min(1,2,3,4,5);                 // 파라미터 내 최소값 반환 // 1
+
+Math.abs(-1);                        // 절대값 반환 // 1
