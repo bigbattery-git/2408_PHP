ab31cdefc04e8db2a9d18cd6cf10047c64ac7b2e
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-11-01 12:22:52 +0900

    axios 및 여러 라이브러리 설치함

--- javascript/js/43.js
+++ javascript/js/43.js
@@ -1,6 +1,7 @@
-const BTN_CALL = document.querySelector('#bnt_call');
+const BTN_CALL = document.querySelector('#btn_call');
 BTN_CALL.addEventListener('click', getList);
 
 function getList(){
-
+	const URL = document.querySelector('#url').value;
+	console.log(URL);
 }
