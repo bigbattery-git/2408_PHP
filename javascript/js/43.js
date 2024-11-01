d304c1d9195f6bf3e73a905b7ea7cc2b868c5614
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-11-01 12:32:19 +0900

    삭제 잼

--- javascript/js/43.js
+++ javascript/js/43.js
@@ -1,7 +1,12 @@
+const axios = import('./node_modules/axios/index.d.cts').default;
+
 const BTN_CALL = document.querySelector('#btn_call');
 BTN_CALL.addEventListener('click', getList);
 
 function getList(){
 	const URL = document.querySelector('#url').value;
-	console.log(URL);
+	
+	axios.get(document.querySelector('#url').value)
+	.then(data => console.log(data.data))
+	.catch(error => console.log(error));
 }
